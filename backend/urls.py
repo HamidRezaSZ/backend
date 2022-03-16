c201490adcf16e22b6ebaa3bf3b5a909532a305a
HamidReza <hr.m.szh@gmail.com>
2022-03-16 14:11:52 +0330

    startapp classify

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
     path("users/", include("users.urls"), name="users"),
+    path("classify/", include("classify.urls"), name="classify"),
 ]
