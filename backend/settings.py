6b2e95d4f571f823e7692fb512bccee5d2896b4b
HamidReza <hr.m.szh@gmail.com>
2022-03-18 22:39:52 +0330

    Add secret key for run project without docker

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 from django.contrib.messages import constants as messages
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-dx64$($05qdjsl8gwy8#1kj92x8#=xcd@_q$#es5dewqciccr9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
